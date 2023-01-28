1539df2fc8de3fbf08c4c585ab4a3115d4bfaf1f
Zax <50070842+Zax809@users.noreply.github.com>
2023-01-28 19:34:19 +0100

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -20,8 +20,8 @@ while True:
     choice = input("Enter choice(1/2/3/4): ")
     if choice in ('1', '2', '3', '4'):
         try:
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
+            1 = float(input("Enter first number: "))
+            2 = float(input("Enter second number: "))
         except ValueError:
             print("Invalid input. Please enter a number.")
             continue
